7316b602fe1253ee199d2128010fd67dde5239b2
aleanrocha <aleanrocha.dev@gmail.com>
2024-07-07 15:44:16 -0300

    fetching user repositories

--- src/screens/Projects/index.tsx
+++ src/screens/Projects/index.tsx
@@ -2,10 +2,24 @@ import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 import { BackBtn } from '../../components/BackBtn'
+import { RepoProps } from '../../types/repo'
 import { ProjectsContainer } from './styles'
 
 export const Projects = () => {
-  const { username } = useParams()
+  const [repo, setRepo] = useState<RepoProps[] | [] | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const { username } = useParams<string>()
+
+  useEffect(() => {
+    const loadRepos = async (username: string) => {
+      setIsLoading(true)
+      const res = await fetch(`https://api.github.com/users/${username}/repos`)
+      const data = await res.json()
+      setIsLoading(false)
+      console.log(data)
+    }
+    if (username) loadRepos(username)
+  }, [username])
 
   return (
     <ProjectsContainer>
