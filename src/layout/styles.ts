725e1ec2520e5628de405b6adea99b87f2479392
aleanrocha <aleanrocha.dev@gmail.com>
2024-07-08 09:19:53 -0300

    improving responsiveness of h1

--- src/layout/styles.ts
+++ src/layout/styles.ts
@@ -7,11 +7,15 @@ export const LayoutContainer = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  padding: 2.4rem;
+  padding: 2.4rem 2.4rem 5.2rem;
 `
 
 export const Title = styled.h1`
   font-size: 4.2rem;
   font-weight: bolder;
   padding: 2.4rem;
+
+  @media screen and (max-width: 580px) {
+    font-size: 2.8rem;
+  }
 `
